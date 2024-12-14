f9a7232a6ef300e5f0f9dcb38e330ae441a3dfc6
dongyun <ehddbs452100@naver.com>
2024-12-14 20:10:06 +0900

    [feat] issue, pr 템플릿 업로드

---
+++ src/test/java/EverSpace/EverSpace_back/EverSpaceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package EverSpace.EverSpace_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverSpaceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
